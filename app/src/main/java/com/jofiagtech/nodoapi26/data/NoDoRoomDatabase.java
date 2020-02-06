3a0255311a68cda7260f906c4e56b9b5630088b2
Jofiag <Aguemon.wahab.97@gmail.com>
2020-02-06 15:09:54 +0100

    Implementation of the Room database.

---
+++ app/src/main/java/com/jofiagtech/nodoapi26/data/NoDoRoomDatabase.java
@@ -0,0 +1,30 @@
+package com.jofiagtech.nodoapi26.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.jofiagtech.nodoapi26.model.NoDo;
+
+@Database(entities = {NoDo.class}, version = 1)
+public abstract class NoDoRoomDatabase  extends RoomDatabase {
+
+    private static volatile NoDoRoomDatabase INSTANCE;
+    public abstract NoDoDao noDoDao();
+
+    static NoDoRoomDatabase getDatabase(Context context){
+        if (INSTANCE == null){
+            synchronized (NoDoRoomDatabase.class){
+                if (INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            NoDoRoomDatabase.class, "noDo_datatbase")
+                            .build();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+}
