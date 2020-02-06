7d4b9ea6f23c2fd58270869636a238be31e00c63
Jofiag <Aguemon.wahab.97@gmail.com>
2020-02-06 10:39:56 +0100

    Setting the DAO (Data Access Object) interface.

--- app/src/main/java/com/jofiagtech/nodoapi26/data/NoDoDao.java
+++ app/src/main/java/com/jofiagtech/nodoapi26/data/NoDoDao.java
@@ -2,14 +2,29 @@ package com.jofiagtech.nodoapi26.data;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
 
 import com.jofiagtech.nodoapi26.model.NoDo;
+
+import java.util.List;
 
 @Dao
 public interface NoDoDao {
     //CRUD
     @Insert
     void insert(NoDo noDo);
+
+    @Query("SELECT * FROM noDo_table ORDER BY noDo_column DESC")
+    List<NoDo> getAllItem();
+
+    @Query("UPDATE noDo_table SET noDo_column = :newNodoText WHERE id = :id")
+    void updateItem(int id, String newNodoText);
+
+    @Query("DELETE FROM noDo_table WHERE id = :idOfItemToDelete")
+    void deleteItem(int idOfItemToDelete);
+
+    @Query("DELETE FROM noDo_table")
+    void deleteAllItem();
 
 
 }
