ccc9976696c51be8687c8f6798930a7b831d5098
Jofiag <Aguemon.wahab.97@gmail.com>
2020-02-09 10:49:02 +0100

    Create the NewNoDoActivity.

---
+++ app/src/main/java/com/jofiagtech/nodoapi26/NewNoDoActivity.java
@@ -0,0 +1,15 @@
+package com.jofiagtech.nodoapi26;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewNoDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_no_do);
+    }
+}
